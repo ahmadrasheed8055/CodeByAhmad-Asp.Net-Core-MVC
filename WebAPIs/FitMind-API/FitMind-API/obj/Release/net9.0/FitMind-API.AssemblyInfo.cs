9cb57c728f0420212fabad98cd527f21c87a8c75
Ahmad Rasheed <ahmad.r.tec@gmail.com>
2025-02-03 22:53:50 +0500

    Something added i don't know

--- WebAPIs/FitMind-API/FitMind-API/obj/Release/net9.0/FitMind-API.AssemblyInfo.cs
+++ WebAPIs/FitMind-API/FitMind-API/obj/Release/net9.0/FitMind-API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitMind-API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e254b9aeb11ef256486680bfcaf9c3643e6ec9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitMind-API")]
